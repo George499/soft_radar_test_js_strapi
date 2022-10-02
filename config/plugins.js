dcfba2520ba04deab5483504dff6bd28344ed881
George499 <gi.32@mail.ru>
2022-10-03 00:38:02 +0300

    updated database

--- config/plugins.js
+++ config/plugins.js
@@ -1,6 +1,20 @@
 const crypto = require("crypto");
 
 module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
+  },
   "users-permissions": {
     config: {
       jwtSecret: env("JWT_SECRET") || crypto.randomBytes(16).toString("base64"),
